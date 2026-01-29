c0519dacf45b43c9be7edae20148e82de0b9591d
Bobinetttte <painauchocolad@gmail.com>
2026-01-29 18:48:47 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+It's been 6 days since I started learning Rust.
+
+## Objectives
+
+The program is designed to serve as a Windows log analyzer.
+
+## Actually
+
+Currently, the program contains hardcoded fake logs to simulate future logic.
+
+## Screenshot
+
+![Screenshot of the program](img/screenshot.png "screenshot.png")
--- src/logger.rs
+++ src/logger.rs
@@ -6,13 +6,16 @@ use colored::Colorize;
 
 pub fn logger(array_events : &Vec<&Event>) {
     let mut stats = stats::Stats::init_stats();
-    println!("===== STAR LOG =====");
+    println!("===== START LOG =====");
     for event in array_events {
         stats.calculate_stats(&event);
         if gravity_rule(&event.gravity_level) {
-            println!("{}", r"\/\/\/ ALERT \/\/\/".red().bold());
+            println!("\n{}", r"<<<< ALERT >>>>".red().bold());
         }
         println!(" id : {} | Type : {}", event.id, event.event_type);
+        if gravity_rule(&event.gravity_level) {
+            println!("{}\n", r"<<<< ALERT >>>>".red().bold());
+        }
     }
     println!("===== END LOG =====");
     let final_stats = stats.get_stats();
