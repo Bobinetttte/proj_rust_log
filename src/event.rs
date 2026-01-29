ce591c53317bbf17631a292f4db11535cb72158b
KALVIN-BTN <kalvin.btn@eduge.ch>
2026-01-29 13:33:19 +0100

    Ajout des events dans le main.rs

---
+++ src/event.rs
@@ -0,0 +1,5 @@
+pub struct Event {
+    pub id : i32,
+    pub event_type : String,
+    pub gravity_level : u8,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+mod event;
+use crate::event::Event;
+
 fn main () {
+
+    /*
+        Events with id, type and gravity level
+    */
+
+    // Debug
+    let event_debug = Event {
+        id : 00,
+        event_type : String::from("Debug"),
+        gravity_level : 0,
+    };
+
+    // Information
+    let event_info = Event {
+        id : 01,
+        event_type : String::from("Information"),
+        gravity_level : 0,
+    };
     
+    // Warning
+    let event_warning = Event {
+        id : 02,
+        event_type : String::from("Warning"),
+        gravity_level : 1,
+    };
+    
+    // Error
+    let event_error = Event {
+        id : 03,
+        event_type : String::from("Error"),
+        gravity_level : 2,
+    };
+    
+    // Critical
+    let event_critical = Event {
+        id : 04,
+        event_type : String::from("Critical"),
+        gravity_level : 3,
+    };
+
+    
+
 }
